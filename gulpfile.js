79efc98a5d95af0e3598b1fb4c4a8b6686b125bf
Cory Gibbons <hello@corygibbons.com>
2015-09-20 10:36:42 -0700

    Make indentation style and size uniform

--- gulpfile.js
+++ gulpfile.js
@@ -9,34 +9,34 @@ var rename = require('gulp-rename');
 
 
 gulp.task('default', ['stylus', 'js'], function() {
-  console.log('Dropping the udon in the pot.');
-  browserSync.init({
-    proxy: "cg.udon"
-  });
-  gulp.watch('src/stylus/*.styl', ['stylus']);
-  gulp.watch('src/js/*.js', ['js']).on('change', browserSync.reload);
-  gulp.watch(['*.php', 'views/*.twig']).on('change', browserSync.reload);
+    console.log('Dropping the udon in the pot.');
+    browserSync.init({
+        proxy: "cg.udon"
+    });
+    gulp.watch('src/stylus/*.styl', ['stylus']);
+    gulp.watch('src/js/*.js', ['js']).on('change', browserSync.reload);
+    gulp.watch(['*.php', 'views/*.twig']).on('change', browserSync.reload);
 });
 
 
 gulp.task('stylus', function() {
-  gulp.src('src/stylus/*.styl')
-  .pipe(stylus({
-    'include css': true,
-    'compress': true
-  }))
-  .pipe(autoprefixer('last 15 version'))
-  .on('error', function(err) { console.log(err.message); })
-  .pipe(gulp.dest('.'))
-  .pipe(browserSync.stream({match: '**/*.css'}));
+    gulp.src('src/stylus/*.styl')
+    .pipe(stylus({
+        'include css': true,
+        'compress': true
+    }))
+    .pipe(autoprefixer('last 15 version'))
+    .on('error', function(err) { console.log(err.message); })
+    .pipe(gulp.dest('.'))
+    .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 
 gulp.task('js', function() {
-  gulp.src('src/js/**/*.js')
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'))
-  .pipe(uglify())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('assets/js'));
+    gulp.src('src/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('assets/js'));
 });
